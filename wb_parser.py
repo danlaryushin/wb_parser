6f4423cf4331256f14afef29781f8406d94a5921
danlaryushin <danlaryushin@gmail.com>
2023-11-20 14:53:11 +0300

    add validation

--- wb_parser.py
+++ wb_parser.py
@@ -11,24 +11,31 @@ def parse(item):
         'spp': SPP
     }
     response = requests.get(MAIN_URL, params=params).json()
-    product_info = response['data']['products'][0]
-    original_price = product_info['priceU']
-    sale_price = product_info['salePriceU']
-    price_info = {
-        'original_price': original_price,
-        'sale_price': sale_price
-    }
-    return price_info
+    try:
+        product_info = response['data']['products'][0]
+        original_price = product_info['priceU']
+        sale_price = product_info['salePriceU']
+        price_info = {
+            'original_price': original_price,
+            'sale_price': sale_price
+        }
+        return price_info
+    except IndexError:
+        print('Товар с таким артикулом не найден.')
+        return
 
 
 def main():
     print('Введите артикул:')
     item = input()
     product_info = parse(item)
-    print(
-        f'Старая цена: {product_info["original_price"]/100} ₽\n'
-        f'Новая цена: {product_info["sale_price"]/100} ₽'
-    )
+    if product_info is None:
+        print('Данные о цене недоступны.')
+    else:
+        print(
+            f'Старая цена: {product_info["original_price"]/100} ₽\n'
+            f'Новая цена: {product_info["sale_price"]/100} ₽'
+        )
 
 
 if __name__ == '__main__':
